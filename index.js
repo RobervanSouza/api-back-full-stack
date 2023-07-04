ce8bb8e93494543f37ddda2e15f3cd148cb798be
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-07-04 11:01:34 -0300

    atualização cors

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 conectDb();
 app.use(express.json());
 app.use(cors());
-// app.use("/api", routes);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 const controller = require('./src/controllers/api.controller');
 
