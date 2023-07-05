5ffcbd750531890d7a4d82ff4d0adf56c53839d6
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-07-05 20:08:18 -0300

    arualização model

--- src/models/api.restaurante.js
+++ src/models/api.restaurante.js
@@ -92,6 +92,10 @@ const gatoSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    descricao: {
+        type: String,
+        required: true,
+    },
     imageUrl: {
         type: String,
         required: true,
