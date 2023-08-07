09bdce715aa1b9e1e02dd28e47443c8f673a2039
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-08-07 11:00:54 -0300

    adicionado email

--- src/models/api.restaurante.js
+++ src/models/api.restaurante.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose"); // inporta o mongoose
 
 const cadastroSchema = new mongoose.Schema({
-    //
+    
     nome: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+    },
+    senha: {
         type: String,
         require: true,
     },
